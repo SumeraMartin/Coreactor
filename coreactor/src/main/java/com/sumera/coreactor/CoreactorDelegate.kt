96419af327bb68ac2a079a640b51883ed255ef5b
Martin Sumera <sumera.martin@gmail.com>
2019-10-06 14:45:57 +0200

    Fix uninitialized property error in CoreactorDelegate

--- coreactor/src/main/java/com/sumera/coreactor/CoreactorDelegate.kt
+++ coreactor/src/main/java/com/sumera/coreactor/CoreactorDelegate.kt
@@ -6,6 +6,7 @@ import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.ViewModelProviders
 import com.sumera.coreactor.contract.action.Action
 import com.sumera.coreactor.contract.state.State
+import com.sumera.coreactor.error.CoreactorException
 import com.sumera.coreactor.internal.ViewFinishingDetector
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.MainScope
@@ -13,33 +14,41 @@ import kotlinx.coroutines.launch
 
 class CoreactorDelegate<STATE : State> : CoroutineScope by MainScope() {
 
-    private lateinit var coreactor: Coreactor<STATE>
+    private var coreactor: Coreactor<STATE>? = null
 
     fun initCoreactor(coreactorOwner: CoreactorOwner<STATE>, lifecycle: Lifecycle, fragment: Fragment): Coreactor<STATE> {
         val factory = coreactorOwner.coreactorFactory
-        val coreactor = ViewModelProviders.of(fragment, factory).get(factory.coreactorClass.java)
-        coreactor.attachView(coreactorOwner.coreactorView)
-        lifecycle.addObserver(coreactor)
-        return coreactor
+        val coreactorClass = factory.coreactorClass.java
+        return ViewModelProviders.of(fragment, factory).get(coreactorClass).apply {
+            coreactor = this
+            attachView(coreactorOwner.coreactorView)
+            lifecycle.addObserver(this)
+        }
     }
 
     fun initCoreactor(coreactorOwner: CoreactorOwner<STATE>, lifecycle: Lifecycle, activity: FragmentActivity): Coreactor<STATE> {
         val factory = coreactorOwner.coreactorFactory
-        val coreactor = ViewModelProviders.of(activity, factory).get(factory.coreactorClass.java)
-        coreactor.attachView(coreactorOwner.coreactorView)
-        lifecycle.addObserver(coreactor)
-        return coreactor
+        val coreactorClass = factory.coreactorClass.java
+        return ViewModelProviders.of(activity, factory).get(coreactorClass).apply {
+            coreactor = this
+            attachView(coreactorOwner.coreactorView)
+            lifecycle.addObserver(this)
+        }
     }
 
     fun sendAction(action: Action<STATE>) = launch {
-        coreactor.sendAction(action)
+        coreactor?.sendAction(action) ?: throwUninitialized()
     }
 
     fun destroyCoreactor(fragment: Fragment) {
-        coreactor.detachView(ViewFinishingDetector.isFinishing(fragment))
+        coreactor?.detachView(ViewFinishingDetector.isFinishing(fragment)) ?: throwUninitialized()
     }
 
     fun destroyCoreactor(activity: FragmentActivity) {
-        coreactor.detachView(ViewFinishingDetector.isFinishing(activity))
+        coreactor?.detachView(ViewFinishingDetector.isFinishing(activity)) ?: throwUninitialized()
+    }
+
+    private fun throwUninitialized(): Nothing {
+        throw CoreactorException("Coreactor is not initialized")
     }
 }
