312391cfb9fe77cfc5edf05b8fa5128260f8871f
Martin Sumera <sumera.martin@gmail.com>
2019-10-06 12:23:04 +0200

    Create coreactor implementation for activity

---
+++ coreactor/src/main/java/com/sumera/coreactor/CoreactorActivity.kt
@@ -0,0 +1,27 @@
+package com.sumera.coreactor
+
+import android.os.Bundle
+import androidx.annotation.LayoutRes
+import androidx.appcompat.app.AppCompatActivity
+import com.sumera.coreactor.contract.state.State
+
+abstract class CoreactorActivity<STATE : State> : AppCompatActivity(), CoreactorOwner<STATE> {
+
+    @LayoutRes abstract fun layoutRes(): Int
+
+    override val coreactorDelegate = CoreactorDelegate<STATE>()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContentView(layoutRes())
+
+        coreactorDelegate.initCoreactor(coreactorOwner = this, lifecycle = lifecycle, activity = this)
+    }
+
+    override fun onDestroy() {
+        coreactorDelegate.destroyCoreactor(activity = this)
+
+        super.onDestroy()
+    }
+}
