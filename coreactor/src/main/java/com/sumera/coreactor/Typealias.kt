5bd0f44a0b4cd1ead6847625da2a568451462ce8
Martin Sumera <sumera.martin@gmail.com>
2019-10-31 08:39:27 +0100

    Remove unused typealiases

--- coreactor/src/main/java/com/sumera/coreactor/Typealias.kt
+++
@@ -1,6 +0,0 @@
-package com.sumera.coreactor
-
-import com.sumera.coreactor.contract.EventOrReducer
-import kotlinx.coroutines.flow.FlowCollector
-
-typealias CoreactorFlow<STATE> = FlowCollector<EventOrReducer<STATE>>
